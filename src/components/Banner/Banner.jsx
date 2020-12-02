c01755622d4e2658d96353807286a6a240b73083
TineManrique <tinemanrique@gmail.com>
2020-12-02 20:35:51 +0800

    first commit

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,62 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { Container, Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
+import { Link } from 'react-scroll';
+import PortfolioContext from '../../context/context';
+
+const Banner = () => {
+  const { hero } = useContext(PortfolioContext);
+  const { title, name, subtitle, cta } = hero;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="hero" className="jumbotron">
+      <Navbar expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Blog</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="banner" style={{display: 'flex'}}>
+        <Container>
+          <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+            <h1 className="hero-title">
+              {title || 'Hi there, I\'m'}{' '}
+              <span className="text-color-main">{name || 'Christine Manrique'}</span>
+              <br />
+              <p>{subtitle || "Add subtitle here with different font and size"}</p>
+            </h1>
+          </Fade>
+          <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+            <p className="hero-cta">
+              <span className="cta-btn cta-btn--hero">
+                <Link to="about" smooth duration={1000}>
+                  {cta || 'Explore more'}
+                </Link>
+              </span>
+            </p>
+          </Fade>
+        </Container>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
